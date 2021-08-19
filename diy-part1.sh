64b34fec7bd4f9df3b175ab8e3fc4239292cdcf9
HUANGDIO <63919781+HUANGDIO@users.noreply.github.com>
2021-08-19 22:13:38 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -17,16 +17,16 @@
 #echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
 #echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
 
-sed -i 's/OpenWrt/G-DOCK/g' package/base-files/files/bin/config_generate
+#sed -i 's/OpenWrt/G-DOCK/g' package/base-files/files/bin/config_generate
 
-sed -i 's/OpenWrt/PandoraBox/g' package/kernel/mac80211/files/lib/wifi/mac80211.sh
+#sed -i 's/OpenWrt/PandoraBox/g' package/kernel/mac80211/files/lib/wifi/mac80211.sh
 
-sed -i 's/192.168.1.1/192.168.2.1/g' package/base-files/files/bin/config_generate
+#sed -i 's/192.168.1.1/192.168.2.1/g' package/base-files/files/bin/config_generate
 
-sed -i 's/encryption=none/encryption=psk2/g' package/kernel/mac80211/files/lib/wifi/mac80211.sh
+#sed -i 's/encryption=none/encryption=psk2/g' package/kernel/mac80211/files/lib/wifi/mac80211.sh
 
-sed -i '/set wireless.default_radio${devidx}.encryption=psk2/a\set wireless.default_radio${devidx}.key=zh970525.' package/kernel/mac80211/files/lib/wifi/mac80211.sh
+#sed -i '/set wireless.default_radio${devidx}.encryption=psk2/a\set wireless.default_radio${devidx}.key=zh970525.' package/kernel/mac80211/files/lib/wifi/mac80211.sh
 
-sed -i 's/OpenWrt/H-Wrt/g' package/lean/default-settings/files/zzz-default-settings
+#sed -i 's/OpenWrt/H-Wrt/g' package/lean/default-settings/files/zzz-default-settings
 
-git clone https://github.com/small-5/luci-app-adblock-plus.git package/lean/luci-app-adblock-plus
+#git clone https://github.com/small-5/luci-app-adblock-plus.git package/lean/luci-app-adblock-plus
